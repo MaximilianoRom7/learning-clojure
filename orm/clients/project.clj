101880dffb80ad024c7cb2b35c1413a3508c3b22
maximilianorom7 <maximilianorom7@gmail.com>
2019-02-26 07:26:12 -0300

    new dependency org.postgresql 45.2.5

--- orm/clients/project.clj
+++ orm/clients/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [korma "0.5.0-RC1"]]
+                 [korma "0.5.0-RC1"]
+                 [org.postgresql/postgresql "45.2.5.jre7"]]
   :ring {:handler basics.core/app-routes
          :auto-reload? true
          :auto-refresh? true}
