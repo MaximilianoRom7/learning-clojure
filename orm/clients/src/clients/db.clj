5f65549208777d55d7986bdb622d4753dd6f89d4
maximilianorom7 <maximilianorom7@gmail.com>
2019-02-26 07:57:16 -0300

    create a basic db schema using korma

---
+++ orm/clients/src/clients/db.clj
@@ -0,0 +1,22 @@
+(ns clients.db
+  (:use [korma.core]
+        [korma.db]))
+
+(defdb pg (postgres
+           {:host "localhost"
+            :port "5432"
+            :db "clojure-clients"
+            :user "postgres"
+            :delimiters ""}))
+
+(defentity clients
+  (entity-fields :id :name :phone :country))
+
+(select clients)
+
+(insert clients
+        (values {:name "Maximiliano Romay"
+                 :phone "+541134179707"
+                 :country "Argentina"}))
+
+(select clients)
