c915247183368a2ec395341e65bd3bc71b378ddb
maximilianorom7 <maximilianorom7@gmail.com>
2019-02-25 21:15:51 -0300

    deleted unnecessary view route_root

--- basics/src/basics/core.clj
+++ basics/src/basics/core.clj
@@ -3,8 +3,6 @@
   (:require [compojure.route :as route])
   (:gen-class))
 
-(defn route_root [name] (str "Hola " name " !"))
-
 (defroutes my_routes
   (GET "/" [] (route_root "Max"))
   (route/resources "/"))
