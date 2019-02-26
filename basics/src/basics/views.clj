4b387311ac534da7816f470a5ac2fa644dd3b6f4
maximilianorom7 <maximilianorom7@gmail.com>
2019-02-25 21:42:17 -0300

    adding css styling into the mix

--- basics/src/basics/views.clj
+++ basics/src/basics/views.clj
@@ -1,9 +1,21 @@
 (ns basics.views
-  (:use hiccup.page hiccup.element))
+  (:use hiccup.page hiccup.element)
+  (:require [clojure.string :as str]))
+
+(defn style-inline [s]
+  (str/join ";" (map #(str (name %) ":" ((keyword %) s)) (keys s))))
+
+
 
 (defn page-index []
   (html5
    [:html
-    [:head]
+    [:head
+     [:style]]
     [:body
-     [:p "Hello World !"]]]))
+     [:div {:style (style-inline {:padding            "20px"
+                                  :background-color   "red"
+                                  :color              "black"
+                                  :font-size          "large"
+                                  :font-weight        "bold"})}
+      [:p "Hello World !"]]]]))
